d659ac61f2d633ddcd889de7fc794ac6104960ef
fourmigh <fourmi_103683@hotmail.com>
2016-12-28 16:33:30 +0800

    新增MD5的库

--- app/src/main/java/org/caojun/salmagundi/string/ConvertUtils.java
+++ app/src/main/java/org/caojun/salmagundi/string/ConvertUtils.java
@@ -2,14 +2,12 @@ package org.caojun.salmagundi.string;
 
 import android.text.TextUtils;
 import android.util.Base64;
-
 import net.sourceforge.pinyin4j.PinyinHelper;
 import net.sourceforge.pinyin4j.format.HanyuPinyinOutputFormat;
 import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
 import net.sourceforge.pinyin4j.format.HanyuPinyinVCharType;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.lang.StringUtils;
-import java.math.BigInteger;
-import java.security.MessageDigest;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -198,19 +196,7 @@ public class ConvertUtils {
      * @return
      */
     public static String toMD5(String text) {
-        try {
-            // 生成一个MD5加密计算摘要
-            MessageDigest md = MessageDigest.getInstance("MD5");
-            // 计算md5函数
-            md.update(text.getBytes());
-            // digest()最后确定返回md5 hash值，返回值为8为字符串。因为md5 hash值是16位的hex值，实际上就是8位的字符
-            // BigInteger函数则将8位的字符串转换成16位hex值，用字符串来表示；得到字符串形式的hash值
-            //signum 该数的正负号（-1 表示负，0 表示零，1 表示正）
-            return new BigInteger(1, md.digest()).toString(16);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return text;
-        }
+        return DigestUtils.md5Hex(text);
     }
 
     public static String toBase64(String text) {
