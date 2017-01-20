c3f21e5deb01ca912866600ced0d6237cb945650
fourmigh <fourmi_103683@hotmail.com>
2017-01-20 10:41:09 +0800

    完善增加傻瓜AI功能，原型功能基本完成

--- app/src/main/java/org/caojun/salmagundi/ai/AIActivity.java
+++ app/src/main/java/org/caojun/salmagundi/ai/AIActivity.java
@@ -10,8 +10,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.socks.library.KLog;
+
 import org.caojun.salmagundi.BaseActivity;
 import org.caojun.salmagundi.R;
+
+import rx.Observable;
+import rx.Subscriber;
 
 /**
  * 傻瓜式AI对话
@@ -20,7 +25,8 @@ import org.caojun.salmagundi.R;
 
 public class AIActivity extends BaseActivity {
 
-    private String[] names = new String[]{};
+    private String[] names;
+    private String[] aiWords;
     private boolean isAITurn = false;//是否轮到AI
     private Button btnSend;
     private EditText etInfo;
@@ -36,6 +42,7 @@ public class AIActivity extends BaseActivity {
         btnSend = (Button) this.findViewById(R.id.btnSend);
 
         names = this.getResources().getStringArray(R.array.ai_name);
+        aiWords = this.getResources().getStringArray(R.array.ai_words);
 
         btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,36 +69,67 @@ public class AIActivity extends BaseActivity {
         });
     }
 
-    private void showInfo(String text) {
-        String info = tvInfo.getText().toString();
-        tvInfo.setText(info + "\n" + text);
+    private void showInfo(final String text, final boolean isEnter) {
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                String info = tvInfo.getText().toString() + text.trim() + (isEnter?"\n":"");
+                tvInfo.setText(info);
+            }
+        });
+
     }
 
-    private void checkMyTurnInput(Editable s) {
-        if(isAITurn) {
-            etInfo.setEnabled(false);
-            btnSend.setEnabled(false);
-            return;
-        }
-        etInfo.setEnabled(true);
-        if(s == null || TextUtils.isEmpty(s.toString())) {
-            btnSend.setEnabled(false);
-        }
-        else {
-            btnSend.setEnabled(true);
-        }
+    private void checkMyTurnInput(final Editable s) {
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if(isAITurn) {
+                    etInfo.setEnabled(false);
+                    btnSend.setEnabled(false);
+                    return;
+                }
+                etInfo.setEnabled(true);
+                if(s == null || TextUtils.isEmpty(s.toString())) {
+                    btnSend.setEnabled(false);
+                }
+                else {
+                    btnSend.setEnabled(true);
+                }
+            }
+        });
     }
 
     private void doMyTurn() {
         String word = etInfo.getText().toString();
-        showInfo(this.getString(R.string.ai_word, names[0], word));
+        showInfo(this.getString(R.string.ai_word, names[0], word), true);
         changeTurn();
         doAITurn(word);
     }
 
     private void doAITurn(String word) {
-        showInfo(this.getString(R.string.ai_word, names[1], "你说啥？"));
-        changeTurn();
+        showInfo(this.getString(R.string.ai_word, names[1], ""), false);
+
+        for(int i = 0;i < aiWords.length;i ++) {
+            final String[] words = aiWords[i].split("->");
+            if(words[0].startsWith(word) || i == aiWords.length - 1) {
+                new Thread() {
+                    @Override
+                    public void run() {
+                        for(int i = 0;i < words[1].length();i ++) {
+                            showInfo(String.valueOf(words[1].charAt(i)), i == words[1].length() - 1);
+                            try {
+                                sleep(100);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        changeTurn();
+                    }
+                }.start();
+                return;
+            }
+        }
     }
 
     private void changeTurn() {
