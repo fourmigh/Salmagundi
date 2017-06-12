96497dd286085db9ab091d279c4404d5a343cf05
CaoJun <fourmi_103683@hotmail.com>
2017-06-12 11:18:08 +0800

    系统信息模块，增加能获取的系统信息

--- app/src/main/java/org/caojun/salmagundi/sysinfo/SysinfoActivity.java
+++ app/src/main/java/org/caojun/salmagundi/sysinfo/SysinfoActivity.java
@@ -3,6 +3,7 @@ package org.caojun.salmagundi.sysinfo;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.telephony.TelephonyManager;
 import android.util.DisplayMetrics;
 import android.widget.ScrollView;
 import android.widget.TextView;
@@ -27,8 +28,9 @@ public class SysinfoActivity extends BaseActivity {
     }
 
     private String getSystemInfo() {
-        DisplayMetrics metrics = getResources().getDisplayMetrics();
         StringBuffer sb = new StringBuffer();
+
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
         sb.append("widthPixels: " + metrics.widthPixels);
         sb.append("\n" + "heightPixels: " + metrics.heightPixels);
         sb.append("\n" + "density: " + metrics.density);
@@ -37,6 +39,7 @@ public class SysinfoActivity extends BaseActivity {
         sb.append("\n" + "xdpi: " + metrics.xdpi);
         sb.append("\n" + "ydpi: " + metrics.ydpi);
         sb.append("\n" + "DPI: " + this.getString(R.string.dpi));
+
         sb.append("\n" + "Build.BOARD: " + Build.BOARD);
         sb.append("\n" + "Build.BOOTLOADER: " + Build.BOOTLOADER);
         sb.append("\n" + "Build.BRAND: " + Build.BRAND);
@@ -60,13 +63,10 @@ public class SysinfoActivity extends BaseActivity {
         sb.append("\n" + "_Build.RADIO: " + Build.RADIO);
         String time = TimeUtils.getTime("yyyy/MM/dd HH:mm:ss", Build.TIME);
         sb.append("\n" + "Build.TIME: " + Build.TIME + "(" + time + ")");
-//        sb.append("\n" + "Build.VERSION.BASE_OS: " + Build.VERSION.BASE_OS);
         sb.append("\n" + "Build.VERSION.CODENAME: " + Build.VERSION.CODENAME);
         sb.append("\n" + "Build.VERSION.INCREMENTAL: " + Build.VERSION.INCREMENTAL);
         sb.append("\n" + "Build.VERSION.RELEASE: " + Build.VERSION.RELEASE);
-//        sb.append("\n" + "Build.VERSION.SECURITY_PATCH: " + Build.VERSION.SECURITY_PATCH);
         sb.append("\n" + "_Build.VERSION.SDK: " + Build.VERSION.SDK);
-//        sb.append("\n" + "Build.VERSION.PREVIEW_SDK_INT: " + Build.VERSION.PREVIEW_SDK_INT);
         sb.append("\n" + "Build.VERSION.SDK_INT: " + Build.VERSION.SDK_INT);
         sb.append("\n" + "Build.VERSION_CODES.BASE: " + Build.VERSION_CODES.BASE);
         sb.append("\n" + "Build.VERSION_CODES.BASE_1_1: " + Build.VERSION_CODES.BASE_1_1);
@@ -104,6 +104,40 @@ public class SysinfoActivity extends BaseActivity {
         for (int i = 0;i < SUPPORTED_ABIS.length;i ++) {
             sb.append("\n" + "Build.SUPPORTED_ABIS[" + i + "]: " + SUPPORTED_ABIS[i]);
         }
+
+        TelephonyManager tm = (TelephonyManager) this.getSystemService(TELEPHONY_SERVICE);
+        sb.append("\n" + "TelephonyManager.getDeviceId(): " + tm.getDeviceId());
+        sb.append("\n" + "TelephonyManager.getDeviceSoftwareVersion(): " + tm.getDeviceSoftwareVersion());
+        sb.append("\n" + "TelephonyManager.getGroupIdLevel1(): " + tm.getGroupIdLevel1());
+        sb.append("\n" + "TelephonyManager.getLine1Number(): " + tm.getLine1Number());
+        sb.append("\n" + "TelephonyManager.getMmsUAProfUrl(): " + tm.getMmsUAProfUrl());
+        sb.append("\n" + "TelephonyManager.getMmsUserAgent(): " + tm.getMmsUserAgent());
+        sb.append("\n" + "TelephonyManager.getNetworkCountryIso(): " + tm.getNetworkCountryIso());
+        sb.append("\n" + "TelephonyManager.getNetworkOperator(): " + tm.getNetworkOperator());
+        sb.append("\n" + "TelephonyManager.getNetworkOperatorName(): " + tm.getNetworkOperatorName());
+        sb.append("\n" + "TelephonyManager.getSimCountryIso(): " + tm.getSimCountryIso());
+        sb.append("\n" + "TelephonyManager.getSimOperator(): " + tm.getSimOperator());
+        sb.append("\n" + "TelephonyManager.getSimOperatorName(): " + tm.getSimOperatorName());
+        sb.append("\n" + "TelephonyManager.getSimSerialNumber(): " + tm.getSimSerialNumber());
+        sb.append("\n" + "TelephonyManager.getSubscriberId(): " + tm.getSubscriberId());
+        sb.append("\n" + "TelephonyManager.getVoiceMailAlphaTag(): " + tm.getVoiceMailAlphaTag());
+        sb.append("\n" + "TelephonyManager.getVoiceMailNumber(): " + tm.getVoiceMailNumber());
+//            sb.append("\n" + "TelephonyManager.canChangeDtmfToneLength(): " + tm.canChangeDtmfToneLength());
+        sb.append("\n" + "TelephonyManager.getCallState(): " + tm.getCallState());
+        sb.append("\n" + "TelephonyManager.getDataActivity(): " + tm.getDataActivity());
+        sb.append("\n" + "TelephonyManager.getDataState(): " + tm.getDataState());
+        sb.append("\n" + "TelephonyManager.getNetworkType(): " + tm.getNetworkType());
+//            sb.append("\n" + "TelephonyManager.getPhoneCount(): " + tm.getPhoneCount());
+        sb.append("\n" + "TelephonyManager.getPhoneType(): " + tm.getPhoneType());
+        sb.append("\n" + "TelephonyManager.getSimState(): " + tm.getSimState());
+//            sb.append("\n" + "TelephonyManager.hasCarrierPrivileges(): " + tm.hasCarrierPrivileges());
+        sb.append("\n" + "TelephonyManager.hasIccCard(): " + tm.hasIccCard());
+//            sb.append("\n" + "TelephonyManager.isHearingAidCompatibilitySupported(): " + tm.isHearingAidCompatibilitySupported());
+        sb.append("\n" + "TelephonyManager.isNetworkRoaming(): " + tm.isNetworkRoaming());
+        sb.append("\n" + "TelephonyManager.isSmsCapable(): " + tm.isSmsCapable());
+//            sb.append("\n" + "TelephonyManager.isTtyModeSupported(): " + tm.isTtyModeSupported());
+//            sb.append("\n" + "TelephonyManager.isVoiceCapable(): " + tm.isVoiceCapable());
+//            sb.append("\n" + "TelephonyManager.isWorldPhone(): " + tm.isWorldPhone());
         return sb.toString();
     }
 }
