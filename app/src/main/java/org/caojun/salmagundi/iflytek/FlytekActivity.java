fedf79764e8fdf4369c70e2a86165a878100d61d
CaoJun <fourmi_103683@hotmail.com>
2017-07-26 17:14:21 +0800

    讯飞语音：增加设置，未完成

--- app/src/main/java/org/caojun/salmagundi/iflytek/FlytekActivity.java
+++ app/src/main/java/org/caojun/salmagundi/iflytek/FlytekActivity.java
@@ -3,9 +3,15 @@ package org.caojun.salmagundi.iflytek;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import com.alibaba.android.arouter.facade.annotation.Route;
+import com.alibaba.android.arouter.launcher.ARouter;
+
 import org.caojun.salmagundi.Constant;
 import org.caojun.salmagundi.R;
 
@@ -15,7 +21,7 @@ import org.caojun.salmagundi.R;
  */
 
 @Route(path = Constant.ACTIVITY_FLYTEK)
-public class FlytekActivity extends Activity {
+public class FlytekActivity extends AppCompatActivity {
 
     private final int[] IDButtons = {R.id.btnEnglish, R.id.btnFrench, R.id.btnRussian, R.id.btnSpanish, R.id.btnHindi, R.id.btnVietnamese};
     private final int[] IDEditTexts = {R.id.etEnglish, R.id.etFrench, R.id.etRussian, R.id.etSpanish, R.id.etHindi, R.id.etVietnamese};
@@ -27,6 +33,8 @@ public class FlytekActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_flytek);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         buttons = new Button[IDButtons.length];
         for (int i = 0;i < IDButtons.length;i ++) {
@@ -36,7 +44,28 @@ public class FlytekActivity extends Activity {
         for (int i = 0;i < IDEditTexts.length;i ++) {
             editTexts[i] = (EditText) findViewById(IDEditTexts[i]);
         }
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            ARouter.getInstance().build(Constant.ACTIVITY_FLYTEK_TTSSETTINGS).navigation();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/org/caojun/salmagundi/iflytek/TtsSettings.java
+++ app/src/main/java/org/caojun/salmagundi/iflytek/TtsSettings.java
@@ -1,13 +1,9 @@
 package org.caojun.salmagundi.iflytek;
 
 import android.os.Bundle;
-import android.preference.EditTextPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
-import android.view.Window;
-
 import com.alibaba.android.arouter.facade.annotation.Route;
-
 import org.caojun.salmagundi.Constant;
 import org.caojun.salmagundi.R;
 
@@ -16,12 +12,12 @@ import org.caojun.salmagundi.R;
  */
 
 @Route(path = Constant.ACTIVITY_FLYTEK_TTSSETTINGS)
-public class TtsSettings extends PreferenceActivity {
+public class TtsSettings extends PreferenceActivity implements Preference.OnPreferenceChangeListener {
 
     public static final String PREFER_NAME = "com.iflytek.setting";
-    private SeekBarPreference mSpeedPreference;
-    private SeekBarPreference mPitchPreference;
-    private SeekBarPreference mVolumePreference;
+//    private SeekBarPreference mSpeedPreference;
+//    private SeekBarPreference mPitchPreference;
+//    private SeekBarPreference mVolumePreference;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,8 +25,13 @@ public class TtsSettings extends PreferenceActivity {
         // 指定保存文件名字
         getPreferenceManager().setSharedPreferencesName(PREFER_NAME);
         addPreferencesFromResource(R.xml.tts_setting);
-        mSpeedPreference = (SeekBarPreference) findPreference("speed_preference");
-        mPitchPreference = (SeekBarPreference) findPreference("pitch_preference");
-        mVolumePreference = (SeekBarPreference) findPreference("volume_preference");
+//        mSpeedPreference = (SeekBarPreference) findPreference("speed_preference");
+//        mPitchPreference = (SeekBarPreference) findPreference("pitch_preference");
+//        mVolumePreference = (SeekBarPreference) findPreference("volume_preference");
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return true;
     }
 }
