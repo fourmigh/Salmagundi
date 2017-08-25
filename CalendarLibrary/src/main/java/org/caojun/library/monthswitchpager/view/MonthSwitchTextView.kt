44ba5ebc038daf5d90d8fedcbc9469f8ed588667
CaoJun <fourmi_103683@hotmail.com>
2017-08-25 10:07:15 +0800

    CalendarLibrary：优化代码，避免频繁更新MonthSwitchTextView

--- CalendarLibrary/src/main/java/org/caojun/library/monthswitchpager/view/MonthSwitchTextView.kt
+++ CalendarLibrary/src/main/java/org/caojun/library/monthswitchpager/view/MonthSwitchTextView.kt
@@ -55,7 +55,7 @@ class MonthSwitchTextView: RelativeLayout {
     }
 
     private fun scrollToPosition() {
-        update()
+//        update()
 //        mMonthRecyclerView!!.scrollToPosition(mPosition)
         mMonthRecyclerView?.smoothScrollToPosition(mPosition)
     }
