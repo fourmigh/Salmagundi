fdc0fb82579720ce5d53487419ab44c4b948e0bf
CaoJun <fourmi_103683@hotmail.com>
2017-09-13 14:55:09 +0800

    分贝侠：修改默认图片资源

--- Decibelman/src/main/java/org/caojun/decibelman/MainActivity.kt
+++ Decibelman/src/main/java/org/caojun/decibelman/MainActivity.kt
@@ -9,16 +9,12 @@ class MainActivity : AppCompatActivity() {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+            R.id.navigation_decibel -> {
+                message.setText(R.string.title_decibel)
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
+            R.id.navigation_map -> {
+                message.setText(R.string.title_map)
                 return@OnNavigationItemSelectedListener true
             }
         }
