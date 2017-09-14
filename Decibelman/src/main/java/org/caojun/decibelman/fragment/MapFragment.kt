66a34123f81f8b718c0807ed0b72828f4b81324f
CaoJun <fourmi_103683@hotmail.com>
2017-09-14 17:06:15 +0800

    增加知识点

--- Decibelman/src/main/java/org/caojun/decibelman/fragment/MapFragment.kt
+++ Decibelman/src/main/java/org/caojun/decibelman/fragment/MapFragment.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.baidu.mapapi.map.BaiduMap
 import kotlinx.android.synthetic.main.fragment_map.*
 import org.caojun.decibelman.R
 
