8a7e37a7f619e4066c23990fc075ef5fd9afe422
CaoJun <fourmi_103683@hotmail.com>
2017-09-30 14:24:38 +0800

    分贝侠：去除注释

--- Decibelman/src/main/java/org/caojun/decibelman/MainApplication.kt
+++ Decibelman/src/main/java/org/caojun/decibelman/MainApplication.kt
@@ -2,7 +2,6 @@ package org.caojun.decibelman
 
 import android.app.Application
 import cn.bmob.v3.Bmob
-import cn.bmob.v3.BmobConfig
 import org.caojun.decibelman.utils.ManifestUtils
 
 /**
@@ -11,17 +10,6 @@ import org.caojun.decibelman.utils.ManifestUtils
 class MainApplication: Application() {
     override fun onCreate() {
         super.onCreate()
-//        val config = BmobConfig.Builder(this)
-//                //设置appkey
-//                .setApplicationId(ManifestUtils.getApplicationMetaData(this, "cn.bmob.v3.appid"))
-//                //请求超时时间（单位为秒）：默认15s
-//                .setConnectTimeout(30)
-//                //文件分片上传时每片的大小（单位字节），默认512*1024
-//                .setUploadBlockSize(1024 * 1024)
-//                //文件的过期时间(单位为秒)：默认1800s
-//                .setFileExpiration(5500)
-//                .build()
-//        Bmob.initialize(config)
         Bmob.initialize(this, ManifestUtils.getApplicationMetaData(this, "cn.bmob.v3.appid"))
     }
 }
