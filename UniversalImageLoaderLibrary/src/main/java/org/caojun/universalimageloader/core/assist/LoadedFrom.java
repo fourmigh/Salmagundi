936e4ef7a0355f55d51f221036705c8ae5b8f9c2
CaoJun <fourmi_103683@hotmail.com>
2017-12-26 16:43:30 +0800

    UniversalImageLoaderLibrary：代码提交，增加对"\"地址的支持

---
+++ UniversalImageLoaderLibrary/src/main/java/org/caojun/universalimageloader/core/assist/LoadedFrom.java
@@ -0,0 +1,10 @@
+package org.caojun.universalimageloader.core.assist;
+
+/**
+ * Source image loaded from.
+ *
+ * @author Sergey Tarasevich (nostra13[at]gmail[dot]com)
+ */
+public enum LoadedFrom {
+    NETWORK, DISC_CACHE, MEMORY_CACHE
+}
