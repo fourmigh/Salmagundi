5bc9939c3ee8617b96e46fc6284239710bb72be5
CaoJun <fourmi_103683@hotmail.com>
2018-01-08 14:52:13 +0800

    摩斯侠：标准电码本录入完成

---
+++ TTShulte/src/main/java/org/caojun/ttshulte/utils/ShulteUtils.kt
@@ -0,0 +1,14 @@
+package org.caojun.ttshulte.utils
+
+/**
+ * Created by CaoJun on 2018-1-8.
+ */
+object ShulteUtils {
+    val Type_Natural = 1//自然数
+    val Type_Square = 2//平方数
+    val Type_Cubic = 3//立方数
+    val Type_Odd = 4//奇数
+    val Type_Even = 5//偶数
+    val Type_Lowercase = 6//小写字母
+    val Type_Uppercase = 7//大写字母
+}
