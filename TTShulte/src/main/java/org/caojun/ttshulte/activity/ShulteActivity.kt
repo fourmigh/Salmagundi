b8fe8eb094f0119154a56c87180054e5bbd8368e
CaoJun <fourmi_103683@hotmail.com>
2018-01-08 11:16:22 +0800

    天天舒尔特表：增加36格

--- TTShulte/src/main/java/org/caojun/ttshulte/activity/ShulteActivity.kt
+++ TTShulte/src/main/java/org/caojun/ttshulte/activity/ShulteActivity.kt
@@ -8,31 +8,24 @@ import android.widget.Button
 import kotlinx.android.synthetic.main.layout_shulte_9.*
 import kotlinx.android.synthetic.main.layout_shulte_16.*
 import kotlinx.android.synthetic.main.layout_shulte_25.*
+import kotlinx.android.synthetic.main.layout_shulte_36.*
 import org.caojun.ttshulte.utils.ViewUtils
 import java.util.*
 
 
 class ShulteActivity : AppCompatActivity() {
 
-    private val LayoutIds = arrayOf(R.layout.layout_shulte_9, R.layout.layout_shulte_16, R.layout.layout_shulte_25)
-//    private val Roots = arrayOf(tlShulte9, tlShulte16, tlShulte25)
-    private var LayoutIndex = 2
+    private val LayoutIds = arrayOf(R.layout.layout_shulte_9, R.layout.layout_shulte_16, R.layout.layout_shulte_25, R.layout.layout_shulte_36)
+    private var LayoutIndex = 3
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_shulte)
         setContentView(LayoutIds[LayoutIndex])
 
-//        val root = (findViewById<ViewGroup>(R.id.tlShulte)).getChildAt(0) as ViewGroup
         val root = getRoot()
-//        layoutParams.width = min
-//        layoutParams.height = min
 
         val buttons = ArrayList<Button>()
         ViewUtils.findButtons(root, buttons)
-//        for (i in 0 until buttons.size) {
-//            buttons[i].text = (i + 1).toString()
-//        }
 
         Collections.shuffle(buttons)
         for (i in 0 until buttons.size) {
@@ -44,7 +37,8 @@ class ShulteActivity : AppCompatActivity() {
         when (LayoutIndex) {
             0 -> return tlShulte9
             1 -> return tlShulte16
-            else -> return tlShulte25
+            2 -> return tlShulte25
+            else -> return tlShulte36
         }
     }
 }
