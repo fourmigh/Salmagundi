b2015d0029678460d9b434e4dcf88fb93068aae4
CaoJun <fourmi_103683@hotmail.com>
2018-01-08 11:03:26 +0800

    天天舒尔特表：布局给为正方形

--- TTShulte/src/main/java/org/caojun/ttshulte/activity/ShulteActivity.kt
+++ TTShulte/src/main/java/org/caojun/ttshulte/activity/ShulteActivity.kt
@@ -2,30 +2,49 @@ package org.caojun.ttshulte.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import org.caojun.ttshulte.R
 import android.view.ViewGroup
+import org.caojun.ttshulte.R
 import android.widget.Button
-import com.socks.library.KLog
+import kotlinx.android.synthetic.main.layout_shulte_9.*
+import kotlinx.android.synthetic.main.layout_shulte_16.*
+import kotlinx.android.synthetic.main.layout_shulte_25.*
 import org.caojun.ttshulte.utils.ViewUtils
 import java.util.*
 
 
 class ShulteActivity : AppCompatActivity() {
 
+    private val LayoutIds = arrayOf(R.layout.layout_shulte_9, R.layout.layout_shulte_16, R.layout.layout_shulte_25)
+//    private val Roots = arrayOf(tlShulte9, tlShulte16, tlShulte25)
+    private var LayoutIndex = 2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_shulte)
-        setContentView(R.layout.layout_shulte_9)
+        setContentView(LayoutIds[LayoutIndex])
 
-        val root = (findViewById<ViewGroup>(android.R.id.content)).getChildAt(0) as ViewGroup
+//        val root = (findViewById<ViewGroup>(R.id.tlShulte)).getChildAt(0) as ViewGroup
+        val root = getRoot()
+//        layoutParams.width = min
+//        layoutParams.height = min
+
         val buttons = ArrayList<Button>()
         ViewUtils.findButtons(root, buttons)
 //        for (i in 0 until buttons.size) {
 //            buttons[i].text = (i + 1).toString()
 //        }
+
         Collections.shuffle(buttons)
         for (i in 0 until buttons.size) {
             buttons[i].text = (i + 1).toString()
+        }
+    }
+
+    private fun getRoot(): ViewGroup {
+        when (LayoutIndex) {
+            0 -> return tlShulte9
+            1 -> return tlShulte16
+            else -> return tlShulte25
         }
     }
 }
---
+++ TTShulte/src/main/java/org/caojun/ttshulte/widget/SquareLinearLayout.kt
@@ -0,0 +1,19 @@
+package org.caojun.ttshulte.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.LinearLayout
+
+/**
+ * Created by CaoJun on 2018-1-8.
+ */
+class SquareLinearLayout: LinearLayout {
+    constructor(context: Context): super(context)
+    constructor(context: Context, attrs: AttributeSet): super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int): super(context, attrs, defStyleAttr)
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val min = Math.min(widthMeasureSpec, heightMeasureSpec)
+        super.onMeasure(min, min)
+    }
+}
