785dbd6255e321d94f40c676356ca6024603d5fc
CaoJun <fourmi_103683@hotmail.com>
2018-01-22 10:51:07 +0800

    天天舒尔特：计时器毫秒数位数增加到3位

--- WidgetLibrary/src/main/java/org/caojun/widget/Stopwatch.kt
+++ WidgetLibrary/src/main/java/org/caojun/widget/Stopwatch.kt
@@ -9,7 +9,6 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Rect
 import android.util.AttributeSet
-import android.util.Log
 import android.util.TypedValue
 import android.view.View
 import android.view.animation.LinearInterpolator
