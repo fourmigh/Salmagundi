0e8d674e73a925d9e31e4599e0d63390570ea9cc
caojun <fourmi_103683@hotmail.com>
2018-03-29 16:27:12 +0800

    转盘电话：改用双缓冲绘制转盘

--- RotaryPhone/src/main/java/org/caojun/rotaryphone/widget/RotaryView.kt
+++ RotaryPhone/src/main/java/org/caojun/rotaryphone/widget/RotaryView.kt
@@ -7,8 +7,7 @@ import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 import android.R.attr.centerY
-
-
+import android.graphics.Bitmap
 
 
 class RotaryView: View {
@@ -18,6 +17,7 @@ class RotaryView: View {
     val AngleInterval = 24
     private var angle0 = Angle0
     private val Numbers = arrayOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "*", "#")
+    private var bitmapRotary: Bitmap? = null
 
     constructor(context: Context?) : this(context, null)
     constructor(context: Context?, attrs: AttributeSet?) : this(context, attrs, 0)
@@ -25,6 +25,48 @@ class RotaryView: View {
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
+        if (bitmapRotary == null) {
+            drawRotary()
+            bitmapRotary = transparentImage(bitmapRotary!!)
+        }
+        val p = Paint().apply {
+            color = Color.BLACK
+            // 设置画笔的锯齿效果
+            isAntiAlias = true
+            textSize = 50f
+            style = Paint.Style.FILL
+            //该方法即为设置基线上那个点究竟是left,center,还是right  这里我设置为center
+            textAlign = Paint.Align.CENTER
+        }
+        val x0 = width.toFloat() / 2
+        val y0 = height.toFloat() / 2
+        val r = Math.min(width, height).toFloat() / 2
+
+        for (i in 0 until Numbers.size) {
+            val angle = Angle0 - AngleInterval * i
+            val radians = toRadians(angle)
+            val x = x0 - (r - RadiusNumber) * Math.cos(radians)
+            val y = y0 - (r - RadiusNumber) * Math.sin(radians)
+            val fontMetrics = p.fontMetrics
+            val top = fontMetrics.top//为基线到字体上边框的距离,即上图中的top
+            val bottom = fontMetrics.bottom//为基线到字体下边框的距离,即上图中的bottom
+            val baseLineY = y - top / 2 - bottom / 2//基线中间点的y轴计算公式
+            canvas.drawText(Numbers[i], x.toFloat(), baseLineY.toFloat(), p)
+        }
+
+        canvas.drawBitmap(bitmapRotary, 0f, 0f, null)
+    }
+
+    private fun toRadians(angel: Int): Double {
+        return Math.PI * angel / 180
+
+    }
+
+    private fun drawRotary() {
+        bitmapRotary = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas()
+        //设置cacheCanvas将会绘制到内存中的cacheBitmap上
+        canvas.setBitmap(bitmapRotary)
         val p = Paint().apply {
             color = Color.BLACK
             // 设置画笔的锯齿效果
@@ -38,7 +80,7 @@ class RotaryView: View {
         p.color = Color.WHITE
         canvas.drawCircle(x0, y0, r - RadiusNumber * 2, p)
 
-        p.color = Color.WHITE
+        p.color = Color.RED
         for (i in 0 until Numbers.size) {
             val angle = angle0 - AngleInterval * i
             val radians = toRadians(angle)
@@ -46,27 +88,28 @@ class RotaryView: View {
             val y = y0 - (r - RadiusNumber) * Math.sin(radians)
             canvas.drawCircle(x.toFloat(), y.toFloat(), RadiusNumber - 20, p)
         }
-
-        p.color = Color.BLACK
-        p.textSize = 50f
-        p.style = Paint.Style.FILL
-        //该方法即为设置基线上那个点究竟是left,center,还是right  这里我设置为center
-        p.textAlign = Paint.Align.CENTER
-        for (i in 0 until Numbers.size) {
-            val angle = Angle0 - AngleInterval * i
-            val radians = toRadians(angle)
-            val x = x0 - (r - RadiusNumber) * Math.cos(radians)
-            val y = y0 - (r - RadiusNumber) * Math.sin(radians)
-            val fontMetrics = p.fontMetrics
-            val top = fontMetrics.top//为基线到字体上边框的距离,即上图中的top
-            val bottom = fontMetrics.bottom//为基线到字体下边框的距离,即上图中的bottom
-            val baseLineY = y - top / 2 - bottom / 2//基线中间点的y轴计算公式
-            canvas.drawText(Numbers[i], x.toFloat(), baseLineY.toFloat(), p)
-        }
     }
 
-    fun toRadians(angel: Int): Double {
-        return Math.PI * angel / 180
+    private fun transparentImage(bmp: Bitmap): Bitmap {
+        val imageWidth = bmp.width
+        val imageHeigth = bmp.height
+        val bmpPixel = IntArray(imageWidth * imageHeigth)
+        bmp.getPixels(bmpPixel, 0, imageWidth, 0, 0, imageWidth, imageHeigth)
+
+        for (i in 0 until imageWidth * imageHeigth) {
+//            if (m_BmpPixel[i] and 0x00ffffff == 0x00ff0000) {
+//                m_BmpPixel[i] = 0x00000000
+//            }
+            val color = bmpPixel[i] and 0x00ffffff
+            if (color == 0x00ffffff || color == 0x00000000) {
+                continue
+            }
+            bmpPixel[i] = 0x00000000
+        }
+
+        bmp.setPixels(bmpPixel, 0, imageWidth, 0, 0, imageWidth, imageHeigth)
+
+        return bmp
 
     }
 }
