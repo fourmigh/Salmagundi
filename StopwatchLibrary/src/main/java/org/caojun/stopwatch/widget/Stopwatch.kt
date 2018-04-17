0a3b35a523718c488d510e0756ca863974051f56
CaoJun <fourmi_103683@hotmail.com>
2018-04-17 13:27:45 +0800

    StopwatchLibrary：优化代码

--- StopwatchLibrary/src/main/java/org/caojun/stopwatch/widget/Stopwatch.kt
+++ StopwatchLibrary/src/main/java/org/caojun/stopwatch/widget/Stopwatch.kt
@@ -19,10 +19,10 @@ import org.caojun.stopwatch.utils.TimeUtil
  */
 class Stopwatch: View {
 
-    private var ringNormalPaint: Paint? = null//圆环小节点Paint
-    private var ringNodePaint: Paint? = null//圆环大节点Paint
-    private var timeTextPaint: Paint? = null//大的时间Paint
-    private var millTextPaint: Paint? = null//毫秒时间Paint
+    private val ringNormalPaint = Paint(Paint.ANTI_ALIAS_FLAG)//圆环小节点Paint
+    private val ringNodePaint = Paint(Paint.ANTI_ALIAS_FLAG)//圆环大节点Paint
+    private val timeTextPaint = Paint(Paint.ANTI_ALIAS_FLAG)//大的时间Paint
+    private val millTextPaint = Paint(Paint.ANTI_ALIAS_FLAG)//毫秒时间Paint
 
     private var ringNormalColor: Int = 0//圆环普通状态颜色
     private var ringRunningColor: Int = 0//圆环运行状态颜色
@@ -48,24 +48,24 @@ class Stopwatch: View {
         timeTextColor = Color.BLACK
         millTextColor = Color.GRAY
 
-        ringNormalPaint = Paint(Paint.ANTI_ALIAS_FLAG)
-        ringNormalPaint!!.style = Paint.Style.STROKE
-        ringNormalPaint!!.color = ringNormalColor
-        ringNormalPaint!!.strokeWidth = ScreenUtil.dpToPx(context, 2)
+//        ringNormalPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+        ringNormalPaint.style = Paint.Style.STROKE
+        ringNormalPaint.color = ringNormalColor
+        ringNormalPaint.strokeWidth = ScreenUtil.dpToPx(context, 2)
 
-        ringNodePaint = Paint(Paint.ANTI_ALIAS_FLAG)
-        ringNodePaint!!.style = Paint.Style.STROKE
-        ringNodePaint!!.color = ringNormalColor
-        ringNodePaint!!.strokeWidth = ScreenUtil.dpToPx(context, 4)
+//        ringNodePaint = Paint(Paint.ANTI_ALIAS_FLAG)
+        ringNodePaint.style = Paint.Style.STROKE
+        ringNodePaint.color = ringNormalColor
+        ringNodePaint.strokeWidth = ScreenUtil.dpToPx(context, 4)
 
-        timeTextPaint = Paint(Paint.ANTI_ALIAS_FLAG)
-        timeTextPaint!!.color = timeTextColor
-        timeTextPaint!!.style = Paint.Style.STROKE
+//        timeTextPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+        timeTextPaint.color = timeTextColor
+        timeTextPaint.style = Paint.Style.STROKE
 //        timeTextPaint!!.textSize = ScreenUtil.dpToPx(context, 70)
 
-        millTextPaint = Paint(Paint.ANTI_ALIAS_FLAG)
-        millTextPaint!!.color = millTextColor
-        millTextPaint!!.style = Paint.Style.STROKE
+//        millTextPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+        millTextPaint.color = millTextColor
+        millTextPaint.style = Paint.Style.STROKE
 //        millTextPaint!!.textSize = ScreenUtil.dpToPx(context, 30)
     }
 
@@ -85,8 +85,8 @@ class Stopwatch: View {
 
         setMeasuredDimension(mWidth, mHeight)
 
-        timeTextPaint?.textSize = (Math.min(mWidth, mHeight) / 4).toFloat()
-        millTextPaint?.textSize = timeTextPaint!!.textSize.times(3) / 7
+        timeTextPaint.textSize = (Math.min(mWidth, mHeight) / 4).toFloat()
+        millTextPaint.textSize = timeTextPaint.textSize.times(3) / 7
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
@@ -104,26 +104,31 @@ class Stopwatch: View {
         var value3 = value - 3
         var value4 = value - 4
         if (time > 0) {
-            if (value == 0) {
-                value1 = MaxMill
-                value2 = MaxMill - 1
-                value3 = MaxMill - 2
-                value4 = MaxMill - 3
-            } else if (value == 1) {
-                value1 = MaxMill
-                value2 = MaxMill - 1
-                value3 = MaxMill - 2
-                value4 = 0
-            } else if (value == 2) {
-                value1 = MaxMill
-                value2 = MaxMill - 1
-                value3 = 0
-                value4 = 1
-            } else if (value == 3) {
-                value1 = MaxMill
-                value2 = 0
-                value3 = 1
-                value4 = 2
+            when (value) {
+                0 -> {
+                    value1 = MaxMill
+                    value2 = MaxMill - 1
+                    value3 = MaxMill - 2
+                    value4 = MaxMill - 3
+                }
+                1 -> {
+                    value1 = MaxMill
+                    value2 = MaxMill - 1
+                    value3 = MaxMill - 2
+                    value4 = 0
+                }
+                2 -> {
+                    value1 = MaxMill
+                    value2 = MaxMill - 1
+                    value3 = 0
+                    value4 = 1
+                }
+                3 -> {
+                    value1 = MaxMill
+                    value2 = 0
+                    value3 = 1
+                    value4 = 2
+                }
             }
         }
 
@@ -132,19 +137,19 @@ class Stopwatch: View {
         for (i in 0..99) {
             if (i % 10 == 0) {
                 if (value == i || value1 == i || value2 == i || value3 == i || value4 == i) {
-                    ringNodePaint!!.color = ringRunningColor
-                    canvas.drawLine((mWidth / 2).toFloat(), 0f, (mWidth / 2).toFloat(), ScreenUtil.dpToPx(context, 10), ringNodePaint!!)
+                    ringNodePaint.color = ringRunningColor
+                    canvas.drawLine((mWidth / 2).toFloat(), 0f, (mWidth / 2).toFloat(), ScreenUtil.dpToPx(context, 10), ringNodePaint)
                 } else {
-                    ringNodePaint!!.color = ringNormalColor
-                    canvas.drawLine((mWidth / 2).toFloat(), 0f, (mWidth / 2).toFloat(), ScreenUtil.dpToPx(context, 10), ringNodePaint!!)
+                    ringNodePaint.color = ringNormalColor
+                    canvas.drawLine((mWidth / 2).toFloat(), 0f, (mWidth / 2).toFloat(), ScreenUtil.dpToPx(context, 10), ringNodePaint)
                 }
             } else {
                 if (value == i || value1 == i || value2 == i || value3 == i || value4 == i) {
-                    ringNormalPaint!!.color = ringRunningColor
-                    canvas.drawLine((mWidth / 2).toFloat(), 0f, (mWidth / 2).toFloat(), ScreenUtil.dpToPx(context, 6), ringNormalPaint!!)
+                    ringNormalPaint.color = ringRunningColor
+                    canvas.drawLine((mWidth / 2).toFloat(), 0f, (mWidth / 2).toFloat(), ScreenUtil.dpToPx(context, 6), ringNormalPaint)
                 } else {
-                    ringNormalPaint!!.color = ringNormalColor
-                    canvas.drawLine((mWidth / 2).toFloat(), 0f, (mWidth / 2).toFloat(), ScreenUtil.dpToPx(context, 6), ringNormalPaint!!)
+                    ringNormalPaint.color = ringNormalColor
+                    canvas.drawLine((mWidth / 2).toFloat(), 0f, (mWidth / 2).toFloat(), ScreenUtil.dpToPx(context, 6), ringNormalPaint)
                 }
             }
             canvas.rotate(360f / 100, (mWidth / 2).toFloat(), (mHeight / 2).toFloat())
@@ -156,9 +161,9 @@ class Stopwatch: View {
         var timeMillString = "000"
         //测量文本
         val timeRect = Rect()
-        timeTextPaint!!.getTextBounds(timeString, 0, timeString.length, timeRect)
+        timeTextPaint.getTextBounds(timeString, 0, timeString.length, timeRect)
         val millTimeRect = Rect()
-        millTextPaint!!.getTextBounds(timeMillString, 0, timeMillString.length, millTimeRect)
+        millTextPaint.getTextBounds(timeMillString, 0, timeMillString.length, millTimeRect)
         //绘制文本
         val textTotalWidth = timeRect.width() + millTimeRect.width()
         val textTotalHeight = timeRect.height()
@@ -166,7 +171,7 @@ class Stopwatch: View {
         canvas.drawText(timeString,
                 ((mWidth - textTotalWidth) / 2).toFloat(),
                 ((mHeight - textTotalHeight) / 2 + timeRect.height()).toFloat(),
-                timeTextPaint!!)
+                timeTextPaint)
 //        if (value < 10) {
 //            if (value > 0) {
 //                timeMillString = "0" + value
@@ -180,7 +185,7 @@ class Stopwatch: View {
         canvas.drawText(timeMillString,
                 (mWidth - textTotalWidth) / 2 + timeRect.width() + ScreenUtil.spToPx(context, 6),
                 ((mHeight - textTotalHeight) / 2 + timeRect.height()).toFloat(),
-                millTextPaint!!)
+                millTextPaint)
     }
 
 
@@ -190,41 +195,39 @@ class Stopwatch: View {
     fun start() {
         if (valueAnimator == null) {
             valueAnimator = ValueAnimator.ofInt(0, MaxMill)
-            valueAnimator!!.interpolator = LinearInterpolator()
-            valueAnimator!!.duration = 1000
-            valueAnimator!!.repeatCount = ValueAnimator.INFINITE//设置无限循环
-            valueAnimator!!.addUpdateListener { animation ->
+            valueAnimator?.interpolator = LinearInterpolator()
+            valueAnimator?.duration = 1000
+            valueAnimator?.repeatCount = ValueAnimator.INFINITE//设置无限循环
+            valueAnimator?.addUpdateListener { animation ->
                 value = animation.animatedValue as Int
                 invalidate()
             }
-            valueAnimator!!.addListener(object : AnimatorListenerAdapter() {
+            valueAnimator?.addListener(object : AnimatorListenerAdapter() {
                 override fun onAnimationRepeat(animation: Animator) {
                     super.onAnimationRepeat(animation)
                     time ++
                     invalidate()
                 }
             })
-            valueAnimator!!.start()
+            valueAnimator?.start()
         } else {
             if (isReset) {
-                valueAnimator!!.start()
+                valueAnimator?.start()
                 isReset = false
             } else {
-                valueAnimator!!.resume()
+                valueAnimator?.resume()
             }
         }
     }
 
     fun stop() {
-        if (valueAnimator != null && valueAnimator!!.isRunning) {
-            valueAnimator!!.pause()
+        if (valueAnimator?.isRunning == true) {
+            valueAnimator?.pause()
         }
     }
 
     fun reset() {
-        if (valueAnimator != null) {
-            valueAnimator!!.cancel()
-        }
+        valueAnimator?.cancel()
         value = 0
         time = 0
         isReset = true
