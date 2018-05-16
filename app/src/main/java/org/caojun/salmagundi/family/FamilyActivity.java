dd1c43b9bd756f1451c00ea5e514ee36e7a8fdce
caojun <fourmi_103683@hotmail.com>
2018-05-16 13:52:37 +0800

    app：修改亲属关系代码BUG

--- app/src/main/java/org/caojun/salmagundi/family/FamilyActivity.java
+++ app/src/main/java/org/caojun/salmagundi/family/FamilyActivity.java
@@ -21,10 +21,46 @@ public class FamilyActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        KLog.d("father", Person.Father.getName());
-        Relation r = RelationUtils.claim(Person.Father, true);
-        KLog.d("father true", r.getName());
-        r = RelationUtils.claim(Person.Father, false);
-        KLog.d("father false", r.getName());
+//        KLog.d("Father", Person.Father.getName());
+//        Relation r = RelationUtils.claim(Person.Father, true);
+//        KLog.d("Father true", r.getName());
+//        r = RelationUtils.claim(Person.Father, false);
+//        KLog.d("Father false", r.getName());
+//        r = RelationUtils.claim(Person.Mother, true);
+//        KLog.d("Mother true", r.getName());
+//        r = RelationUtils.claim(Person.Mother, false);
+//        KLog.d("Mother false", r.getName());
+//        r = RelationUtils.claim(Person.Son, true);
+//        KLog.d("Son true", r.getName());
+//        r = RelationUtils.claim(Person.Son, false);
+//        KLog.d("Son false", r.getName());
+//        r = RelationUtils.claim(Person.Daughter, true);
+//        KLog.d("Daughter true", r.getName());
+//        r = RelationUtils.claim(Person.Daughter, false);
+//        KLog.d("Daughter false", r.getName());
+//        r = RelationUtils.claim(Person.EBrother, true);
+//        KLog.d("EBrother true", r.getName());
+//        Relation r = RelationUtils.claim(Person.EBrother, false);
+//        KLog.d("EBrother false", r.getName());
+//        Relation r = RelationUtils.claim(Person.YBrother, true);
+//        KLog.d("YBrother true", r.getName());
+//        Relation r = RelationUtils.claim(Person.YBrother, false);
+//        KLog.d("YBrother false", r.getName());
+//        Relation r = RelationUtils.claim(Person.ESister, true);
+//        KLog.d("ESister true", r.getName());
+//        Relation r = RelationUtils.claim(Person.ESister, false);
+//        KLog.d("ESister false", r.getName());
+        Relation r = RelationUtils.claim(Person.YSister, true);
+        KLog.d("YSister true", r.getName());
+        r = RelationUtils.claim(Person.YSister, false);
+        KLog.d("YSister false", r.getName());
+        r = RelationUtils.claim(Person.Husband, true);
+        KLog.d("Husband true", r.getName());
+        r = RelationUtils.claim(Person.Husband, false);
+        KLog.d("Husband false", r.getName());
+        r = RelationUtils.claim(Person.Wife, true);
+        KLog.d("Wife true", r.getName());
+        r = RelationUtils.claim(Person.Wife, false);
+        KLog.d("Wife false", r.getName());
     }
 }
--- app/src/main/java/org/caojun/salmagundi/family/data/Person.java
+++ app/src/main/java/org/caojun/salmagundi/family/data/Person.java
@@ -13,7 +13,7 @@ public class Person {
     public static final Relation EBrother = new Relation(0, true, Relation.XD, "兄", true);
     public static final Relation YBrother = new Relation(0, true, Relation.XD, "弟", false);
     public static final Relation ESister = new Relation(0, false, Relation.XD, "姐", true);
-    public static final Relation YSister = new Relation(0, true, Relation.XD, "妹", false);
+    public static final Relation YSister = new Relation(0, false, Relation.XD, "妹", false);
     public static final Relation Husband = new Relation(0, true, Relation.FQ, "夫");
     public static final Relation Wife = new Relation(0, false, Relation.FQ, "妻");
 
